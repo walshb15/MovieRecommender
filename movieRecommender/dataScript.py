480611631864ac19e73b3ee88c2ff76a35fa5d41
Parker Kirby <kirbypar@msu.edu>
2020-11-24 10:01:48 -0500

    Added two tables to the database, ratings and movies

---
+++ movieRecommender/dataScript.py
@@ -0,0 +1,53 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+#test = ['3932', 'Invisible Man, The (1933)', 'Horror|Sci-Fi\n']
+#testLine = "INSERT into ratings(movieId, title, genre) VALUES(" + test[0] + ",'" + test[1] +"'," + test[2]+" );"
+#print(testLine)
+database = "db.sqlite3"
+con = create_connection(database)
+cursor = con.cursor()
+cursor.execute("SELECT * FROM movies")
+print(cursor.fetchall())
+#f = open("C:\\Users\\kirbypar\\Desktop\\CSE482\Ml-1m\\movies.dat")
+#for line in f:
+#    line = line.replace("\n", "")
+#    line = line.split("::")
+#    insert = "INSERT into movies(movieId, title, genre) VALUES((?), (?), (?))"
+#    cursor.execute(insert, (line[0], line[1], line[2]))
+#con.commit()
+#sql_create_projects_table = """ CREATE TABLE movies (
+#                                    movieId integer,
+#                                    title text,
+#                                    genre text
+#                                ); """
+#conn = create_connection(database)
+#create_table(conn, sql_create_projects_table)
+
